681b3e6c7deb80912a9fd0ba75653862294655bb
ModernGalahad <m.git@dv.dx.ro>
2024-08-07 18:11:54 +0300

    Finished the pig game. Tutorial followed:https://www.youtube.com/watch?v=21FnnGKSRZo I also added some touches to make it my own...not too big of a difference but well it's a start.

--- pig_game.py
+++ pig_game.py
@@ -10,8 +10,8 @@ def roll():
 
 
 
-pl = input("\nHow many players will be playing? At least 2 players and at most 4 are required to start the game. \n")
-nrplayers = int(pl)
+players = input("\nEnter the number of players (2-4):\n")
+nrplayers = int(players)
 
 if nrplayers > 4 or nrplayers < 2:
     sys.exit("Invalid answer. Please try again.")
@@ -22,27 +22,35 @@ max_score = 50
 while max(playerscores) < max_score:
     
     for playeridx in range(nrplayers):
-        print("\n Player",playeridx + 1, " turn has just started.")
-        while True: 
-            current_score = 0
+        print("\n" + "=".center(24,"="))
+        print("Player",playeridx + 1, "\'s turn has just started.")
+        print("Your total score is:", playerscores[playeridx], "\n")
 
-            should_roll = input("Would you like to roll (y)?")
-            if should_roll.lowercase != "y":
+        current_score = 0
+        
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != "y":
                 break
             roll_value = roll()
 
             if roll_value == 1:
-                print("Oh no. You got a 1 and lost your score...")
+                print("\nOh no. You got a 1 and lost your score. Turn done!")
+                current_score = 0
                 break
             else:
                 current_score += roll_value
                 print("Your rolled a: ", roll_value)
 
-            print("Your score is: ", current_score)
-        playerscores[playeridx] += current_score    
-        print("Your total score: ", playerscores[playeridx])
+            print("Your score is:", current_score)
+
+            playerscores[playeridx] += current_score    
+            print("Your total score:", playerscores[playeridx])
 
 
+max_score = max(playerscores)
+winning_idx = playerscores.index(max_score)
+print("\n Player number", winning_idx + 1, "won with a score of", max_score)
 
 
 
